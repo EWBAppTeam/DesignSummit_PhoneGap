ea8762d0ba625e08fce11a68904fe0e856bccc19
Daniel Freeman <dr.freeman@me.com>
2016-06-03 13:19:43 +1000

    Edit Menu Text
    Changed Info to Lower-Cass instead of CAPS

--- APP/platforms/browser/www/script.js
+++ APP/platforms/browser/www/script.js
@@ -1,9 +1,3 @@
 
 document.getElementById("navBar1").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>HOME</a></li>" +
-"<li><a href='info.html'>INFO</a></li>" +
-"<li><a href='schedule.html'>SCHEDULE</a></li>" +
-"<li><a href='map.html'>MAP</a></li>" +
-"<li><a href='feedback.html'>FEEDBACK</a></li>" +
-"</ul>";
+"<h1><a href='index.html'>BACK</a></h1>";
